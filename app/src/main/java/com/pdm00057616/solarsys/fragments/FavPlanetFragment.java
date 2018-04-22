c9af75d5442686d38480b7870c055ff473c1be50
petrlr14 <00057616@uca.edu.sv>
2018-04-22 00:52:44 -0600

    se crearon los fragmenos a mostrar en fav y planets

---
+++ app/src/main/java/com/pdm00057616/solarsys/fragments/FavPlanetFragment.java
@@ -0,0 +1,44 @@
+package com.pdm00057616.solarsys.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pdm00057616.solarsys.MainActivity;
+import com.pdm00057616.solarsys.R;
+import com.pdm00057616.solarsys.adapters.FavPlanetAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class FavPlanetFragment extends Fragment {
+
+    private ViewPager vp;
+
+    @BindView(R.id.recyclerviewFav)
+    RecyclerView recyclerView;
+
+    public FavPlanetFragment() {
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fav_planet_fragment, container, false);
+        ButterKnife.bind(this, view);
+        vp = getActivity().findViewById(R.id.viewpager);
+        FavPlanetAdapter adapter = new FavPlanetAdapter(MainActivity.planetsFav, vp);
+        recyclerView.setAdapter(adapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/pdm00057616/solarsys/fragments/PlanetFragment.java
@@ -0,0 +1,47 @@
+package com.pdm00057616.solarsys.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pdm00057616.solarsys.MainActivity;
+import com.pdm00057616.solarsys.R;
+import com.pdm00057616.solarsys.adapters.PlanetAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PlanetFragment extends Fragment {
+
+
+    private ViewPager vp;
+
+    @BindView(R.id.recyclerviewPlanet)
+    RecyclerView recyclerView;
+
+    public PlanetFragment() {
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.planet_fragment, container, false);
+        ButterKnife.bind(this, view);
+        vp = getActivity().findViewById(R.id.viewpager);
+        PlanetAdapter adapter = new PlanetAdapter(MainActivity.planetArrayList, vp);
+        recyclerView.setAdapter(adapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        return view;
+    }
+
+
+}
